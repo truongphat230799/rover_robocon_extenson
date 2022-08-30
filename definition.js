855f24556788fbfc40d2953ce39042c838a6e3e0
truongphat230799 <60847675+truongphat230799@users.noreply.github.com>
2022-08-30 13:54:30 +0700

    Update definition.js

--- definition.js
+++ definition.js
@@ -65,6 +65,6 @@ Blockly.Blocks['rover_turn_until_line_detected'] = {
   Blockly.Python['rover_follow_line_until'] = function(block) {
     Blockly.Python.definitions_['import_rover'] = 'from rover import *';
     var request = Blockly.Python.valueToCode(block,  'request', Blockly.Python.ORDER_ATOMIC);
-    var code = "follow_forward_line()\n" + "wait_for(" + request+")\n" + "rover.stop()\n";
+    var code = "follow_forward_line()\n" + "wait_for(lambda:" + request+")\n" + "rover.stop()\n";
     return code;
   };

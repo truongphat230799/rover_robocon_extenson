ee042b897b3f5142704b1929a4c6045587586aef
truongphat230799 <taphat1753@gmail.com>
2023-09-18 10:59:51 +0700

    add gripper grab & release

--- definition.js
+++ definition.js
@@ -228,6 +228,47 @@ Blockly.Python["control_servo"] = function (block) {
   var dropdown_pin = block.getFieldValue('pin');
   var servo_speed = Blockly.Python.valueToCode(block, 'speed', Blockly.Python.ORDER_ATOMIC);
   // TODO: Assemble Python into code variable.
-  var code = "moveGripper(" + dropdown_pin + ", " + angle_servo + ", " + servo_speed+")\n";
+  var code = "set_servo_position(" + dropdown_pin + ", " + angle_servo + ", " + servo_speed+")\n";
   return code;
+};
+
+Blockly.Blocks['servo_collect'] = {
+  init: function () {
+    this.jsonInit(
+      {
+        "type": "servo_collect",
+        "message0": "robot %1 quà",
+        "args0": [     
+          {
+            type: "field_dropdown",
+            name: "action",
+            options: [
+            ["gắp", "collect"],
+            ["thả", "release"],
+            ]
+          }     
+        ],
+        "inputsInline": true,
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": ColorBlock,
+        "tooltip": "",
+        "helpUrl": ""
+      }
+    );
+  }
+};
+
+Blockly.Python["control_servo"] = function (block) {
+  Blockly.Python.definitions_['import_rover'] = 'from rover import *';
+  Blockly.Python.definitions_['import_robocon'] = 'from robocon import *';
+  var dropdown_type = block.getFieldValue('action');
+  // TODO: Assemble Python into code variable.
+  var code = "";
+  if (dropdown_type == 'collect')
+    code = "set_servo_position(1, 0, 80)\n" + "time.sleep_ms(1000)\n" + "set_servo_position(2, 90, 80)" + "time.sleep_ms(1000)\n";
+  else
+    code = "set_servo_position(2, 0, 80)\n" + "time.sleep_ms(1000)\n" + "set_servo_position(1, 90, 80)" + "time.sleep_ms(1000)\n";
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.Python.ORDER_NONE];
 };
